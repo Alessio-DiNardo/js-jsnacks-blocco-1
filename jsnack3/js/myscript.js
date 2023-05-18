a6296f8ec0b0033df7d3244276cfa64a7a8f169d
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-18 16:31:16 +0200

    Add array and finish snack 3

--- jsnack3/js/myscript.js
+++ jsnack3/js/myscript.js
@@ -1,3 +1,13 @@
 //Snack 3:
 //Crea un array vuoto.
 //Chiedi per 6 volte all’utente di inserire un numero,  se è dispari inseriscilo nell’array.
+
+let array = [];
+
+const userNumber = 6;
+
+
+for( let i = 0; i < userNumber; i++ ){
+    array.push(prompt("insert one number"))
+}
+console.log(array);
