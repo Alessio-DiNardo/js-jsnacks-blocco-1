da58bd24aeb448254ef73f24eed44ae2cbe2601f
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-18 17:22:56 +0200

    es 4 add 4 num

--- jsnack4/js/myscript.js
+++ jsnack4/js/myscript.js
@@ -1,3 +1,12 @@
 //Snack 4:
 //Chiedi un numero di 4 cifre all’utente
 //e calcola la somma di tutte le cifre che compongono il numero.
+
+const numberOne = parseInt(prompt("inserisci un numero"))
+const numberTwo = parseInt(prompt("inserisci un numero"))
+const numberThree = parseInt(prompt("inserisci un numero"))
+const numberFour = parseInt(prompt("inserisci un numero "))
+
+const totale = numberOne + numberTwo + numberThree + numberFour;
+
+console.log(totale);
