fc1b7bee0b6d7ce371278f1b100a8d854ee31f6b
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-18 14:32:49 +0200

    first commit

---
+++ jsnack1/js/myscript.js
@@ -0,0 +1,18 @@
+//Snack 1:
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+
+
+const numberOne = prompt ("inserisci un numero");
+const numberTwo = prompt ("inserisci un numero");
+const numberThree = prompt ("inserisci un numero");
+const numberFour = prompt ("inserisci un numero");
+const numberFive = prompt ("inserisci un numero");
+const numberSix = prompt ("inserisci un numero");
+const numberSeven = prompt ("inserisci un numero");
+const numberEight = prompt ("inserisci un numero");
+const numberNine = prompt ("inserisci un numero");
+const numberTen = prompt ("inserisci un numero");
+
+console.log("numberOne" + "numberTwo" + "numberThree" + "numberFour" + "numberFive" + "numberSix" + "numberSeven" +"numberEight" +"numberNine" +"numberTen")
+
