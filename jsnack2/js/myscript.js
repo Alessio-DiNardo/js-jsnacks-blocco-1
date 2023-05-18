101ea03c1702eca1cd53a3283a2fa0006c745471
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-18 15:15:55 +0200

    start code with array

--- jsnack2/js/myscript.js
+++ jsnack2/js/myscript.js
@@ -1,2 +1,10 @@
 //Snack 2:
 //In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+
+const partyList = [ "Asuna", "Doraimon", "Mara Maionchi", "Fedez", "Ash", "Pika",];
+
+const userList = prompt("inserisci il tuo nome");
+
+let isNameFound = false;
+
